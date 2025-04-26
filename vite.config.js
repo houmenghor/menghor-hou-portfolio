911a1d1592f40e5e25c17d192e2d4ea1faec259e
menghorHou <mhour3055@gmail.com>
2025-04-26 15:42:01 +0700

    My Portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/menghor-hou-portfolio/', // 💥 add this line
   plugins: [react()],
   server: {
     port: 3000
